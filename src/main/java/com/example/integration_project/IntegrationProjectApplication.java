01fe44b3e32082bad4fad566bf6cddf0c0913103
Cris01000011 <168916184+Cris01000011@users.noreply.github.com>
2024-06-27 18:53:47 -0600

    project

---
+++ src/main/java/com/example/integration_project/IntegrationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.integration_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/integration_project/service/UserService.java
@@ -0,0 +1,17 @@
+package com.example.integration_project.service;
+
+import com.example.integration_project.model.User;
+import java.util.List;
+
+public interface UserService {
+
+    User createUser(User user);
+
+    User getUserById(String userId);
+
+    List<User> getAllUsers();
+
+    User updateUser(String userId, User user);
+
+    void deleteUser(String userId);
+}
